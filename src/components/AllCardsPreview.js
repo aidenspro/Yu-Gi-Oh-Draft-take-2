f2f7af241be17470d666fcea31d92fed833be197
aidenspro <aidenspro@gmail.com>
2022-03-03 13:12:48 -0500

    2 files modified

--- src/components/AllCardsPreview.js
+++ src/components/AllCardsPreview.js
@@ -5,7 +5,7 @@ var images = [];
 export default function AllCardsPreview(props) {
 var tempArray = props.cards[0];
 console.log(props.cards[0])
-  if(tempArray != 55144522)
+  if(tempArray != 55144522 && tempArray>0)
     images.push( <img className={".placeholder-plain-preview"} 
     src={
       'https://storage.googleapis.com/ygoprodeck.com/pics/' +
