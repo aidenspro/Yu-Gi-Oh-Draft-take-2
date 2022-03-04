5c9b181efd9e954f50481249a2466a2c516c5d5a
aidenspro <aidenspro@gmail.com>
2022-03-03 19:40:41 -0500

    2 files modified

--- src/components/AllCardsPreview.js
+++ src/components/AllCardsPreview.js
@@ -5,8 +5,22 @@ var images = [];
 export default function AllCardsPreview(props) {
 var tempArray = props.cards[0];
 console.log(props.cards[0])
+var divRef = useRef();
+var imgRef = useRef();
+
+const hoverEnter = (event) => {
+imgRef.current.className = "placeholder-plain" + " " + "zoom";
+}
+//set the card preview to not visible
+const hoverExit = (event) => {
+  imgRef.current.className = "placeholder-plain"
+}
+
   if(tempArray != 55144522 && tempArray>0)
-    images.push( <img className={"placeholder-plain-preview" + " " + "zoom"} 
+    images.push( <div id={tempArray} ref={divRef} onMouseEnter={hoverEnter} onMouseOut={hoverExit}> 
+    <img 
+    ref={imgRef}
+    className={"placeholder-plain" } 
     src={
       'https://storage.googleapis.com/ygoprodeck.com/pics/' +
       props.cards[0] +
@@ -19,7 +33,9 @@ console.log(props.cards[0])
     draggable="false"
     position="relative"
     alt={props.cards[1]}
-  />)
+  />
+  </div>
+  )
     
 
   return(images)
