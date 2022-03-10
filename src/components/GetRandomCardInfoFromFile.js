ca882c19c6a25185285c7d917f9cd4fd69a28f1a
aidenspro <aidenspro@gmail.com>
2022-03-09 22:03:19 -0500

    1 file modified, 1 added

---
+++ src/components/GetRandomCardInfoFromFile.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import MakeCardGivenInfo from "../components/MakeCardGivenInfo"
+
+function getJson() {
+  const [card, setCard] = useState([]);
+
+  useEffect(() => {
+    fetch('https://db.ygoprodeck.com/api/v7/randomcard.php')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //return obj = data;
+        //console.log(data);
+        setCard(data); // setting obj using setObj
+      })
+      .then(() => {
+        return card;
+      });
+  }, []);
+
+  return card;
+}
+
+//assigns array based on jSon info
+function returnInfo(cardJSON){
+  var cardInfo = [];
+  
+  cardInfo[0]=cardJSON.id;
+  cardInfo[1]=cardJSON.name;
+  cardInfo[2]=cardJSON.desc;
+  cardInfo[3]=cardJSON.type;
+  cardInfo[4]=cardJSON.race;
+
+  cardInfo[5]=cardJSON.archetype;
+  cardInfo[6]=cardJSON.atk;
+  cardInfo[7]=cardJSON.def;
+  cardInfo[9]=cardJSON.level;
+  cardInfo[10]=cardJSON.attribute
+  
+
+  if(cardInfo[6] >= 0 ) {
+  cardInfo[11]= "ATK/DEF : " + cardInfo[6] + "/" + cardInfo[7];
+  }else{
+    cardInfo[11]=" "
+  }
+  
+
+  
+
+
+  return cardInfo;
+}
+
+export default function cardInfo() {
+return (returnInfo(getJson()));
+}
