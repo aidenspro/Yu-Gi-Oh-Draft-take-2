3b130dcd906793d9c3f069d8aae099e3ce496ddd
aidenspro <aidenspro@gmail.com>
2022-03-09 22:05:07 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from "react";
-import MakeCardGivenInfo from "./components/MakeCardGivenInfo"
-import GetRandomCardInfo from "./components/GetRandomCardInfo"
-import CardInspector from "./components/CardInspector"
-import DraftDisplay from "./components/DraftDisplay"
-import TestMove from "./components/TestMove"
-import "./style.css";
+import React from 'react';
+import MakeCardGivenInfo from './components/MakeCardGivenInfo';
+import GetRandomCardInfo from './components/GetRandomCardInfo';
+import CardInspector from './components/CardInspector';
+import DraftDisplay from './components/DraftDisplay';
+import CardLibrary from './components/CardLibrary';
+import './style.css';
 
 export default function App() {
-  var infoArray = [48214588,"Rookie fur Hire"];
+  var infoArray = [48214588, 'Rookie fur Hire'];
+  console.log(CardLibrary);
 
-
-  return (
-      <DraftDisplay />
-  );
+  return <DraftDisplay />;
 }
