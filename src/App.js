7432131a30b14c72f84c97d5f72a99b27ddd3402
aidenspro <aidenspro@gmail.com>
2022-03-09 22:23:21 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import MakeCardGivenInfo from './components/MakeCardGivenInfo';
 import GetRandomCardInfo from './components/GetRandomCardInfo';
 import CardInspector from './components/CardInspector';
 import DraftDisplay from './components/DraftDisplay';
-import { CardLibrary } from './components/CardLibrary';
+import CardLibrary from './components/CardLibrary';
 import './style.css';
+
+function Notes() {
+  const [text, setText] = React.useState();
+  fetch(CardLibrary)
+    .then((response) => response.text())
+    .then((textContent) => {
+      setText(textContent);
+    });
+  return text || 'Loading...';
+}
 
 export default function App() {
   var infoArray = [48214588, 'Rookie fur Hire'];
-  console.log(CardLibrary);
 
   return <DraftDisplay />;
 }
